f3f3696ec11c9a4b9f85d64de7767e037f366d33
koleso8 <dev.koleso@gmail.com>
2024-08-12 14:38:04 +0300

    add library

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <p>proj</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
---
+++ src/services/searchAPI.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export const fethData = async () => {
+  return await axios.get();
+};
